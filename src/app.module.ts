4dc6f250984de56046dd3caa26c5fb7f5e884389
chrischen86 <chrischen86@gmail.com>
2021-05-17 02:32:13 -0400

    Remove unused module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConquestModule } from './conquest/conquest.module';
 import { SocketioModule } from './socketio/socketio.module';
-import { DalModule } from './dal/dal.module';
 
 @Module({
-  imports: [ConquestModule, SocketioModule, DalModule],
+  imports: [ConquestModule, SocketioModule],
   controllers: [AppController],
   providers: [AppService],
 })
