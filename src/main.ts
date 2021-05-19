29235e8ecf51bf8d2b5ed8549c1c7bbe63d932a7
chrischen86 <chrischen86@gmail.com>
2021-05-18 23:53:22 -0400

    Add hot reload for quicker develop cycle

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SocketIoAdapter } from './socketio/socketio.adapter';
 
+declare const module: any;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useWebSocketAdapter(new SocketIoAdapter(app, true));
   await app.listen(5000);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
