6f7d2d3749346bc773093a43481c82dd20f3f12a
chrischen86 <chrischen86@gmail.com>
2021-06-05 23:00:01 -0400

    Allow port to come from environment

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
-  await app.listen(5000);
+  await app.listen(process.env.PORT || 5000);
 
   if (module.hot) {
     module.hot.accept();
