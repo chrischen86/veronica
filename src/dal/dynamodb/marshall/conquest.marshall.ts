48e0899c9cda6a9f6f7f087f0c1a10c436336320
chrischen86 <chrischen86@gmail.com>
2021-06-15 23:22:25 -0400

    Fixed marshalling conquest to include global secondary index

--- src/dal/dynamodb/marshall/conquest.marshall.ts
+++ src/dal/dynamodb/marshall/conquest.marshall.ts
@@ -2,11 +2,14 @@ import { Conquest } from 'src/conquest/interfaces/conquest.interface';
 
 export const marshallConquest = (conquest: Conquest) => {
   const { id, allianceId, startDate, endDate } = conquest;
+  const startDateString = startDate.toISOString();
   return {
     ...marshallConquestKey(id),
+    GSI1PK: { S: `ALLIANCE#${allianceId}` },
+    GSI1SK: { S: startDateString },
     id: { S: id },
     allianceId: { S: allianceId },
-    startDate: { S: startDate.toISOString() },
+    startDate: { S: startDateString },
     endDate: { S: endDate.toISOString() },
   };
 };
