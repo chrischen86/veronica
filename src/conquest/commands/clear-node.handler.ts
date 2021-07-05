a1b07add7a23f376330d46c1d53b25dc4191abdb
chrischen86 <chrischen86@gmail.com>
2021-07-05 10:16:28 -0400

    Do not assume the person removing a node assignmment is the person assigned when updating stats

--- src/conquest/commands/clear-node.handler.ts
+++ src/conquest/commands/clear-node.handler.ts
@@ -2,6 +2,7 @@ import { CommandHandler, EventBus, ICommandHandler } from '@nestjs/cqrs';
 import { NodeRepository } from '../../dal/repository/node.repository';
 import { NodeClearedEvent } from '../../shared/events/node-cleared.event';
 import { NodeUpdatedEvent } from '../../shared/events/node-updated.event';
+import { Context } from '../../shared/interfaces/context.interface';
 import ClearNodeCommand from './clear-node.command';
 
 @CommandHandler(ClearNodeCommand)
@@ -15,7 +16,25 @@ export class ClearNodeHandler implements ICommandHandler<ClearNodeCommand> {
     console.log('ClearNodeCommand...');
 
     const { conquestId, phaseId, zoneId, nodeId, context } = command;
+    const nodeToClear = await this.repository.findOneOnZoneById(
+      conquestId,
+      phaseId,
+      zoneId,
+      nodeId,
+    );
+
     await this.repository.clearOwner(conquestId, phaseId, zoneId, nodeId);
-    this.eventBus.publish(new NodeClearedEvent(conquestId, nodeId, context));
+
+    const { ownerId, ownerName } = nodeToClear;
+    const { allianceId, allianceName } = context;
+    const removedUserContext = new Context(
+      ownerId,
+      ownerName,
+      allianceId,
+      allianceName,
+    );
+    this.eventBus.publish(
+      new NodeClearedEvent(conquestId, nodeId, removedUserContext),
+    );
   }
 }
